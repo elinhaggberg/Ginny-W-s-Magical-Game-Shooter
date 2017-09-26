d1df86259ae3b97db415140b8ac72ea10a719160
Elin Häggberg <elin.haggberg@gmail.com>
2017-09-26 18:25:47 +0200

    Added assets

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -4,12 +4,13 @@ var mainGameState = { };
 // Add the preload function 
 mainGameState.preload = function () {
     console.log("Pre-loading the Game");
+    this.game.load.image("game-bg", "assets/images/space-bg.jpg")
 }
 
 //Add the create function 
 
 mainGameState.create = function() {
-    
+    game.add.sprite(0, 0, 'game-bg');  
 }
 
 // Add the update function 
