8bf26dbd87068a93e7a662f83bea4a036fcd13f8
Elin Häggberg <elin.haggberg@gmail.com>
2017-09-27 11:42:16 +0200

    Added background music

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -6,10 +6,10 @@ mainGameState.preload = function () {
     console.log("Pre-loading the Game");
     this.game.load.image("game-bg", "assets/images/ginny-w-bg.jpg")
     this.game.load.image("player", "assets/images/ginny-w-player.png")
+    this.game.load.audio("game-music", "assets/music/harry-potter-8bit.WAV")
 }
 
 //Add the create function 
-
 mainGameState.create = function() {
     
 //Load the Physics system
@@ -18,6 +18,12 @@ game.physics.startSystem(Phaser.Physics.ARCADE);
     
 //Add background
     game.add.sprite(0, 0, 'game-bg');
+    
+//Add the background music
+    this.music = game.add.audio('game-music');
+    this.music.play();
+    this.music.volume = 0.5;
+    this.music.loop = true;
     
 //Add coordinate-variables that uses game width and height    
     var x = game.width * 0.5;
@@ -31,7 +37,7 @@ game.physics.startSystem(Phaser.Physics.ARCADE);
 //Enable movement of the Player 
     game.physics.arcade.enable(this.playerSprite);
     
-//CONTROLS – creating the game controls for the player 
+//CONTROLS – activating the game controls for the player 
     this.cursors = game.input.keyboard.createCursorKeys();
     
     
@@ -40,15 +46,27 @@ game.physics.startSystem(Phaser.Physics.ARCADE);
 // Add the update function 
 mainGameState.update = function() {
     
+//Setting up the right and left arrow controls   
      if (this.cursors.right.isDown) {
-        this.playerSprite.body.velocity.x = 200;
+        this.playerSprite.body.velocity.x = 300;
     } else if (this.cursors.left.isDown) {
-        this.playerSprite.body.velocity.x = -200;
+        this.playerSprite.body.velocity.x = -300;
     } else {
         this.playerSprite.body.velocity.x= 0;
     }
     
+//Confiding ship to game screen
+    if ( (this.playerSprite.x > game.width) && ( this.playerSprite.body.velocity.x > 0 ) ) {
+        this.playerSprite.body.velocity.x = 0;
+    }
+
+    if ( (this.playerSprite.x < 0 ) && (this.playerSprite.body.velocity.x < 0) ) {
+        this.playerSprite.body.velocity.x = 0;
+    }
+
+// STAY INSIDE THE FUNCTION, ELIN!!!!!  
 }
+
 
 
 
