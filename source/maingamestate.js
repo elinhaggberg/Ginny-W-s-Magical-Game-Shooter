1dd25fad6ccdafff8b1ee78a1e69507027a1b098
Elin Häggberg <elin.haggberg@gmail.com>
2017-09-27 10:55:26 +0200

    Added controls

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -12,19 +12,41 @@ mainGameState.preload = function () {
 
 mainGameState.create = function() {
     
-    var x = game.width * 0.5;
-    var y = game.height * 0.8;
+//Load the Physics system
+
+game.physics.startSystem(Phaser.Physics.ARCADE);
     
+//Add background
     game.add.sprite(0, 0, 'game-bg');
     
+//Add coordinate-variables that uses game width and height    
+    var x = game.width * 0.5;
+    var y = game.height * 0.8;
+
+//Add the player-sprite and set its anchorpoint and scale
     this.playerSprite = game.add.sprite(x,y,'player');
     this.playerSprite.anchor.setTo(0.5,0.5);
     this.playerSprite.scale.setTo(0.5);   
+    
+//Enable movement of the Player 
+    game.physics.arcade.enable(this.playerSprite);
+    
+//CONTROLS – creating the game controls for the player 
+    this.cursors = game.input.keyboard.createCursorKeys();
+    
     
 }
 
 // Add the update function 
 mainGameState.update = function() {
+    
+     if (this.cursors.right.isDown) {
+        this.playerSprite.body.velocity.x = 200;
+    } else if (this.cursors.left.isDown) {
+        this.playerSprite.body.velocity.x = -200;
+    } else {
+        this.playerSprite.body.velocity.x= 0;
+    }
     
 }
 
