cd41167e1b937fd74ad3f430867d815ae1ba91f4
Elin Häggberg <elin.haggberg@gmail.com>
2017-09-29 17:14:48 +0200

    New instructions for mobile

--- source/instructionsstate.js
+++ source/instructionsstate.js
@@ -3,15 +3,22 @@ var instructionsState = {};
 instructionsState.preload = function () {
     //Load all assets
     this.game.load.image("instruction-bg","assets/images/instruction-bg.jpg");
+    this.game.load.image("instruction-bg-mobile","assets/images/instruction-bg-mobile.jpg")
     this.game.load.image("snitch", "assets/images/snitch.png");
     this.game.load.image("exit-button","assets/images/exit-button.png");
 };
 
 instructionsState.create = function () {
     //Add the background
-    background = game.add.sprite(0,0,'instruction-bg');
-    background.scale.setTo(0.5);
     
+    if (game.device.desktop) {
+        background = game.add.sprite(0,0,'instruction-bg');
+        background.scale.setTo(0.5);
+    } else {
+        background = game.add.sprite(0,0,'instruction-bg-mobile');
+        background.scale.setTo(0.5);
+    }
+
     button = game.add.button(10,10,'exit-button',actionOnClick,this,2,1,0);
     button.scale.setTo(0.7);
     
--- source/maingamestate.js
+++ source/maingamestate.js
@@ -102,7 +102,7 @@ mainGameState.create = function() {
     };
     
     if ( !(game.device.desktop) ) {
-        this.instructionsButton = game.add.button(game.width / 2,game.height - 30, 'instructions-button',this.actionOnClick, this, 2, 1, 0);
+        this.instructionsButton = game.add.button(game.width / 2,game.height - 20, 'instructions-button',this.actionOnClick, this, 2, 1, 0);
         this.instructionsButton.anchor.setTo(0.5,0.5);
         this.instructionsButton.scale.setTo(0.8);
     };
